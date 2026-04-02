abe476b6c8519810e222eaab4667ecd0ac13da20
Shivamsiingh79 <shivamsiingh01@gmail.com>
2026-04-02 13:17:04 +0530

    Updated production API URL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://prepwise-gfqa.onrender.com",
     withCredentials:true
 })
 export async function register({username,email,password}){
